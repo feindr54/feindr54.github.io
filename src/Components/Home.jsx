e7a8912ecda9207566193249d20243f651f602d6
Gao Changxiang <gao654@purdue.edu>
2022-01-23 16:37:09 -0500

    Updated personal logo and added home page js file

---
+++ src/Components/Home.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import './Pages.css';
+
+function Home() {
+    return (
+        <div className = 'home'>
+            
+        </div>
+    );
+}
