bd9d0aa92f6a5af8681b880e05eb0aa24440b325
Gao Changxiang <gao654@purdue.edu>
2023-07-16 22:57:23 -0400

    reorganized icons to public

--- my-app/components/header.js
+++ my-app/components/header.js
@@ -1,18 +1,18 @@
 import ImageLink from './imagelink'
 import NavLink from './navlink'
 
-import githubPic from '../assets/github.svg'
-import emailPic from '../assets/mail.svg'
-import LPic from '../assets/linkedin.svg'
+// import githubPic from '/github.svg'
+// import emailPic from '/mail.svg'
+// import LPic from '/linkedin.svg'
 
 export default function Header() {
   return (
     <header className='flex flex-row border-b border-white'>
       <div className='pl-8'>
         <ul className='flex flex-row items-center gap-8 m-3'>
-          <ImageLink src={emailPic} href='mailto:cxgao1729@gmail.com'/>
-          <ImageLink src={githubPic} href='https://www.github.com/feindr54'/>
-          <ImageLink src={LPic} href={'https://www.linkedin.com/in/changxiang-gao/'}/>
+          <ImageLink src="/mail.svg" href='mailto:cxgao1729@gmail.com'/>
+          <ImageLink src="/github.svg" href='https://www.github.com/feindr54'/>
+          <ImageLink src="/linkedin.svg" href={'https://www.linkedin.com/in/changxiang-gao/'}/>
         </ul>
       </div>
       <div className='grow mr-10'>
